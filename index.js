cdf9e5ad9303d5ccf3c5a8bfc84b1c49bef32e74
Batuhan Icoz <batuhanicoz@gmail.com>
2016-06-05 00:21:53 +0300

    converted the main code to ES6 & deleted repeating code

--- index.js
+++ index.js
@@ -1,52 +1,42 @@
 const parallel = require('async').parallel
 const ipRangeCheck = require('ip-range-check')
-const azure = require('./lib/azure')
-const aws = require('./lib/aws')
-const gce = require('./lib/gce')
 
-module.exports = function (ip, done) {
-  var name = 'unknown'
-  parallel([
-    function (cb) {
-      check(ip, azure(), function (err, _name) {
-        if (err) return cb(err)
-        else {
-          if (_name) name = _name
-          return cb()
-        }
-      })
-    },
-    function (cb) {
-      check(ip, aws(), function (err, _name) {
-        if (err) return cb(err)
-        else {
-          if (_name) name = _name
-          return cb()
-        }
-      })
-    },
-    function (cb) {
-      check(ip, gce(), function (err, _name) {
-        if (err) return cb(err)
-        else {
-          if (_name) name = _name
-          return cb()
-        }
-      })
-    }
-  ], function (err) {
-    if (err) return done(err)
-    else return done(null, name)
-  })
-}
+const providers = [
+    require('./lib/azure'), 
+    require('./lib/aws'), 
+    require('./lib/gce')
+];
 
-function check (ip, cloud, cb) {
-  cloud.list(function (err, ranges) {
-    if (err) return cb(err)
-    else if (ipRangeCheck(ip, ranges)) {
-      return cb(null, cloud.name)
-    } else {
-      return cb()
+module.exports = (ip, done) => {
+    let name = 'unknown';
+
+    function checkersGenerator(ip) {
+        const checkers = [];
+        providers.forEach(provider => {
+            checkers.push(cb => {
+                check(ip, provider(), (err=null, _name=null) => {
+                    if (err) return cb(err);
+                    if (_name) name = _name;
+                    return cb(null);
+                });
+            });
+        });
+        return checkers;
     }
-  })
+
+    parallel(checkersGenerator(ip), err => {
+        if (err) return done(err)
+        else return done(null, name)
+    })
+};
+
+function check(ip, cloud, cb) {
+    cloud.list((err, ranges) => {
+        if (err) return cb(err)
+        else if (ipRangeCheck(ip, ranges)) {
+            return cb(null, cloud.name)
+        } else {
+            return cb()
+        }
+    })
 }
